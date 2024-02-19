806618886af834bf52808c124d10ea5ddffd5dd3
gmutafov <georgimutafovbg@gmail.com>
2024-02-19 21:18:15 +0200

    .

---
+++ JS_Front-End/test.js
@@ -0,0 +1,21 @@
+let a = 5;
+let b = 6;
+console.log(a + b);
+
+var c = 30;
+var n = 40;
+console.log(c - n);
+
+const pi = 3.14;
+
+
+function add(first, second){
+    console.log(first + second);
+}
+add(3, pi);
+
+console.log('The number pi is: ' + pi + '!');
+
+console.log(`The number pi is: ${pi}!`);
+
+console.log(pi.toFixed(2));
