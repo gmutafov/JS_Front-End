4be13c3298dbadc94211feed89d9579a5622b2d1
Georgi Mutafov <145926316+gmutafov@users.noreply.github.com>
2024-02-22 16:28:20 +0200

    .

---
+++ JS_Front-End/JS Syntax Fundamentals - Exercises/road-radar.js
@@ -0,0 +1,41 @@
+function solve(km, road){
+    let speedLimit = 0;
+    
+    switch(road){
+        case 'motorway':
+        speedLimit = 130;
+        break;
+        case 'interstate':
+        speedLimit = 90;
+        break;
+        case 'city':
+        speedLimit = 50;
+        break;
+        
+        case 'residential':
+        speedLimit = 20;
+        break;
+        
+    }
+    const speedDiff = km - speedLimit;
+    
+    if (speedDiff <= 0){
+        return console.log(`Driving ${km} km/h in a ${speedLimit} zone`);
+    }
+    let status = '' 
+    if (speedDiff < 20){
+        status = 'speeding';
+    } else if ( speedDiff < 40){
+        status = 'excessive speeding';
+    } else{
+        status = 'reckless driving';
+    }
+    
+    console.log(`The speed is ${speedDiff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+}
+
+
+solve(40, 'city');
+solve(21, 'residential');
+solve(120, 'interstate');
+solve(200, 'motorway');
