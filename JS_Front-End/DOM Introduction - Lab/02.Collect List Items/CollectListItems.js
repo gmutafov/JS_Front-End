ea7f2a9a2b4159ed1b5962b53774be5d32a0bdf8
Georgi Mutafov <145926316+gmutafov@users.noreply.github.com>
2024-03-19 08:01:30 +0200

    Exercises

--- JS_Front-End/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ JS_Front-End/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let itemNodes = document.querySelectorAll('ul#items li')
+    let textarea = document.querySelector('#result')
+
+    for (let node of itemNodes){
+        textarea.value += node.textContent + "\n"
+    }
 }
--- JS_Front-End/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ JS_Front-End/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let a = document.getElementById('num1').value;
+    let b = document.getElementById('num2').value;
+    let sum = Number(a) + Number(b)
+
+    document.getElementById('sum').value = sum;
 }
