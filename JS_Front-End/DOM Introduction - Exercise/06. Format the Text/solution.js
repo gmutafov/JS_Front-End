ff615627b454380f65340f530e7fdc9ca402b1f5
Georgi Mutafov <145926316+gmutafov@users.noreply.github.com>
2024-03-21 15:47:50 +0200

    .

--- JS_Front-End/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS_Front-End/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,15 +1,24 @@
 function solve() {
-    const outputEl = document.getElementById('output')
-    const textareaEl = document.getElementById('input');
-    const text = textareaEl.value;
+    const outputElement = document.getElementById('output');
+    const textareaElement = document.getElementById('input');
 
-    const result = text.split('.').filter(sentence => !!sentence).reduce((result, sentence, i) => {
-        const resultIndex = Math.floor(i/3);
-        if (!resultIndex){
-            result[resultIndex] = []
-        }
-        result[resultIndex].push(sentence.trim());
-        return result;
-    }, []).map(sentences => `<p>${sentences.join('. ')}</p>`).join('\n');
-    outputEl.innerHTML = result;
+    const text = textareaElement.value;
+
+    const result = text
+        .split('.')
+        .filter(sentence => !!sentence)
+        .reduce((result, sentence, i) => {
+            const resultIndex = Math.floor(i / 3);
+            if (!result[resultIndex]) {
+                result[resultIndex] = []
+            }
+
+            result[resultIndex].push(sentence.trim());
+
+            return result;
+        }, [])
+        .map(sentences => `<p>${sentences.join('. ')}.</p>`)
+        .join('\n');
+
+    outputElement.innerHTML = result;
 }
